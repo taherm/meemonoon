68030f5ec6a7774ab79555accdc42ef2e2d9fbec
Ahmed Shalaby <ahmedmahmoudit@gmail.com>
2016-10-11 01:10:02 +0300

    continue update files to V5.3

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <div class="page-header-inner ">
         <!-- BEGIN LOGO -->
         <div class="page-logo">
-            <a href="index.html">
+            <a href="{{url('backend')}}">
                 <img src="{{asset('meem/backend/img/meem-logo3.png')}}" alt="logo" class="logo-default" /> </a>
             <div class="menu-toggler sidebar-toggler">
                 <span></span>
