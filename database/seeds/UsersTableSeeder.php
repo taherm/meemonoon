ddacd2c7a4a4e84d03f7849d585c554a33607e72
uusa35 <uusa35@gmail.com>
2016-10-23 15:15:15 +0000

    updating the db seed for productioin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,11 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        if (app()->environment() === 'production') {
-            factory('App\Src\User\User', 1)->create(['email' => 'admin@test.com', 'username' => 'admin']);
-        } else {
-            factory('App\Src\User\User', 1)->create(['email' => 'admin@test.com', 'username' => 'admin']);
-            factory('App\Src\User\User', 4)->create();
-        }
+        factory('App\Src\User\User', 1)->create(['email' => 'admin@test.com', 'username' => 'admin']);
+        factory('App\Src\User\User', 1)->create();
     }
 }
