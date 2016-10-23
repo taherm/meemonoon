1f9a48bc1e956240b87a62cf62037ec965c68503
uusa35 <uusa35@gmail.com>
2016-10-23 15:51:47 +0000

    updating the Size Create Form

--- resources/views/backend/modules/size/create.blade.php
+++ resources/views/backend/modules/size/create.blade.php
@@ -4,11 +4,11 @@
     {{ Form::open(['route' => 'backend.size.store','method'=>'POST','class'=>'form-horizontal']) }}
     <div class="form-body">
         <div class="form-group">
-            <label class="col-md-2 control-label">color name:
+            <label class="col-md-2 control-label">Size name:
                 <span class="required"> * </span>
             </label>
             <div class="col-md-5">
-                {!! Form::text('color', old('color') ,['class' => 'form-control','required']) !!}
+                {!! Form::text('size', old('size') ,['class' => 'form-control','required']) !!}
             </div>
         </div>
         <div class="form-group">
