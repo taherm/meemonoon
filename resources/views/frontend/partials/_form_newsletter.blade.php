9a1c6cfc54757cd12e68abefa24caaaff684706a
uusa35 <uusa35@gmail.com>
2016-10-26 16:32:46 +0000

    fixing small bug within the newsletter form

--- resources/views/frontend/partials/_form_newsletter.blade.php
+++ resources/views/frontend/partials/_form_newsletter.blade.php
@@ -4,20 +4,20 @@
     <label for="inputEmail1" class="col-lg-4 control-label"></label>
 
     <div class="col-lg-12">
-        {!! Form::text('email',['class' => 'form-control', 'placeholder' => trans('general.email'), 'required']) !!}
+        {!! Form::text('email',['class' => 'form-control', 'placeholder' => 'email', 'required']) !!}
     </div>
 </div>
 <div class="form-group">
     <label for="text1" class="col-lg-4 control-label"></label>
 
     <div class="col-lg-12">
-        {!! Form::text('name',['class' => 'form-control', 'placeholder' => trans('general.name'), 'required']) !!}
+        {!! Form::text('name',['class' => 'form-control', 'placeholder' => 'name', 'required']) !!}
     </div>
 </div>
 <div class="form-group">
     <div class="col-lg-6 col-lg-push-3 text-center">
         <button type="submit"
-                class="btn btn-success text-center">{!! trans('general.subscribe')  !!}</button>
+                class="btn btn-success text-center">subscribe</button>
     </div>
 </div>
 {!! Form::close() !!}
