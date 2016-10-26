15e708f4d4c0b718d0936f76b1c2ff7f311c35d2
usama ahmed <uusa35@gmail.com>
2016-10-26 01:05:03 -0400

    translations from the server

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -1,15 +1,28 @@
 <?php
 
 return array (
-  'home' => 'home',
-  'submit' => 'submit',
+  'messsage' => 
+  array (
+    'warning' => 
+    array (
+      'create_new' => 'messsage.warning.create_new	',
+    ),
+  ),
+  'address_ar' => 'Address Arabic',
+  'active' => 'active',
+  'address' => 'address',
+  'aboutus' => 'About us',
+  'faqs' => 'FAQ',
+  'all_rights_reserved' => 'All Rights Reserved	',
+  'home' => 'Home',
   'categories' => 'categories',
+  'new_arrival' => 'New Arivals',
+  'on_sale_products' => 'On Sale Products',
   'language' => 'language',
   'arabic' => 'arabic',
   'english' => 'english',
-  'new_arrival' => 'New Arivals',
-  'on_sale_products' => 'On Sale Products',
-  'best_sale_products' => 'Best Sale Products',
+  'add_to_cart' => 'add to cart',
   'view_product_details' => 'view details',
-  'aboutus' => 'نبذة عنا',
+  'submit' => 'submit',
+  'best_sale_products' => 'Best Sale Products',
 );
