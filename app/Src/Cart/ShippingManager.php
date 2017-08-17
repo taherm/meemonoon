29fe34cff6dc601af4484b81fa3726274e99b104
usama ahmed <uusa35@gmail.com>
2017-08-17 14:13:54 +0300

    working on aramex

--- app/Src/Cart/ShippingManager.php
+++ app/Src/Cart/ShippingManager.php
@@ -79,6 +79,9 @@ class ShippingManager
         try {
             $countriesSoapClient = new \SoapClient(env('ARAMEX_COUNTRY_URL'), array('trace' => 1));
             $country = $countriesSoapClient->FetchCountry($country);
+            ob_start();
+            phpinfo();
+            $info = ob_end_clean();
             dd($country);
             if (!is_null($country->Country->Name)) {
                 $calcSoapClient = new \SoapClient(env('ARAMEX_CALC_URL'), array('trace' => 1));
