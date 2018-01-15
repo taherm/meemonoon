ea589d29b2bde9541da4a1bf84c9b189127eb098
usama ahmed <uusa35@gmail.com>
2018-01-15 14:57:20 +0300

    first commit

--- app/Src/User/User.php
+++ app/Src/User/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-//    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password', 'remember_token'];
     protected $casts = [
         'active' => 'boolean',
     ];
