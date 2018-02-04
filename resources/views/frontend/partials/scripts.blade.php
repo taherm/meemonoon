47d3809a30c369a552ade7448222fb5c8cc335c1
usama ahmed <uusa35@gmail.com>
2018-02-04 21:07:14 +0300

    done with upload shit

--- resources/views/frontend/partials/scripts.blade.php
+++ resources/views/frontend/partials/scripts.blade.php
@@ -22,6 +22,16 @@
     });
 
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113550846-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-113550846-1');
+</script>
+
 
 
 <!-- Placed js at the end of the document so the pages load faster -->
